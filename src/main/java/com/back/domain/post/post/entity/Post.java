cfc80023d618cdafebb015eeb546c86b6ca14dd5
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-04 15:38:57 +0900

    24강 날짜 추가

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -3,6 +3,8 @@ package com.back.domain.post.post.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 /*
 post를 2개 쓴이유 글 안에 여러개의 글이 있기 때문에
 댓글, 게시글 등등
@@ -19,12 +21,16 @@ public class Post {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO_INCREMENT
 //	private final int id;
 	private int id;
+	private LocalDateTime createDate;
+	private LocalDateTime modifyDate;
 	private String title;
 	@Column(columnDefinition = "TEXT") //필드 수정 char->text : db를 꼭 삭제해야한다
 	private String content;
 
 	public Post(String title, String content) {
 //		this.id = 0; //fianl을 넣을시 초기값에 id의 값을 넣어줘야하는 불편함
+		this.createDate = LocalDateTime.now();
+		this.modifyDate = this.createDate;
 		this.title = title;
 		this.content = content;
 	}
