bb4fa9a776a9c6791f2547afbe3ff82f8e1cdb80
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-04 12:28:24 +0900

    baseData 추가

---
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -0,0 +1,23 @@
+package com.back.global.initData;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//전체적으로 영향역을 미치는 곳 global 파트로 넣는다
+@Configuration
+public class BaseInitData {
+	@Bean
+	ApplicationRunner baseInitDataApplicationRunner() {
+		return args -> {
+			System.out.println("기본 데이터가 초기화되었습니다.");
+			//TODO db 접속할 로직을 넣을 예정
+		};
+	}
+}
+
+/*
+ @Configuration : 빈의 일종, 내부에 @Bean 메서드를 가질 수 있다
+ @Bean : 메서드는 스프링 부트가 시작할 때 자동으로 실행된다.
+ ApplicationRunner : 빈에 등록되면 자동으로 실행되는 성질을 가지고 있다.
+* */
