25d8607f88853b91a751eb2a6a2f896dfbebc28e
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-04 12:38:41 +0900

    posetrepository 도입

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,7 @@
 package com.back.global.initData;
 
+import com.back.domain.post.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +9,15 @@ import org.springframework.context.annotation.Configuration;
 //전체적으로 영향역을 미치는 곳 global 파트로 넣는다
 @Configuration
 public class BaseInitData {
+	@Autowired
+	private PostRepository postRepository;
+
 	@Bean
 	ApplicationRunner baseInitDataApplicationRunner() {
 		return args -> {
 			System.out.println("기본 데이터가 초기화되었습니다.");
 			//TODO db 접속할 로직을 넣을 예정
+			postRepository.count();
 		};
 	}
 }
