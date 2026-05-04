ec9c86d37ad61679a942e3d23eb141f6719d99d9
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-04 14:39:39 +0900

    18강 특정 필드만 초기화

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,10 +1,8 @@
 package com.back.domain.post.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 /*
@@ -15,10 +13,12 @@ post를 2개 쓴이유 글 안에 여러개의 글이 있기 때문에
 @Entity //밑의 구성대로 DB 테이블이 형성 되어야한다.
 @Getter
 @Setter
+@RequiredArgsConstructor //final 붙은 애들만 초기 셋팅 값으로 지정한다.
 public class Post {
 	@Id //PK
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO_INCREMENT
 	private int id;
-	private String title;
-	private String content;
+	private final String title;
+	@Column(columnDefinition = "TEXT") //필드 수정 char->text : db를 꼭 삭제해야한다
+	private final String content;
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -18,11 +18,11 @@ public class BaseInitData {
 		return args -> {
 			System.out.println("기본 데이터가 초기화되었습니다.");
 			//TODO db 접속할 로직을 넣을 예정
-//			postRepository.count();
-			//이곳에다가 넣게 되면 재 실행 될때마다 계속 생성될것이다.
-			Post post = new Post();
-			post.setTitle("제목 1");
-			postRepository.save(post);
+			if (postRepository.count() > 0) return;
+
+			Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
+			Post post2 = postRepository.save(new Post("제목 2", "내용 2"));
+
 		};
 	}
 }
