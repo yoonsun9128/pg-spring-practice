0c897113d3ac0a7744e99a21477999cc46e04c6a
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-07 23:13:55 +0900

    transactional 적용

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -19,7 +20,6 @@ import java.util.Optional;
 public class BaseInitData {
 
 	private final PostService postService; //RequiredArgsConstructor + final
-	private int callCount = 0; //주입받을라고 쓴게 아니라 내부적으로 사용 하기 위해
 
 	/*
 	* AllArgsConstructor
@@ -37,9 +37,10 @@ public class BaseInitData {
 			work1();
 			work2();
 
-			callCount++;
 		};
 	}
+
+	@Transactional
 	void work1() {
 		if (postService.count() > 0) return;
 
@@ -50,6 +51,7 @@ public class BaseInitData {
 		System.out.println("기본 데이터가 초기화되었습니다.");
 	}
 
+	@Transactional(readOnly = true)
 	void work2() {
 		Optional<Post> opPost1 = postService.findById(1);
 		// SELECT * FROM post WHERE id = 1;
