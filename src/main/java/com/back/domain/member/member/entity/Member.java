61e0754b191e9edb1c0ace4d62faebfbfc6fbe09
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-05-08 17:30:59 +0900

    member 추가

---
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -0,0 +1,22 @@
+package com.back.domain.member.member.entity;
+
+import com.back.domain.post.post.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Entity
+@Setter
+@ToString
+@NoArgsConstructor
+public class Member extends BaseEntity {
+	@Column(nullable = true)
+	private String usename;
+	private String password;
+	private String nickname;
+
+}
---
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.back.domain.member.member.service;
+
+import com.back.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+	private final MemberRepository memberRepository;
+}
